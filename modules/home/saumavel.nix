be0a9b3e6de1997e9be06613be77e99346423aee
saumavel <kari@genkiinstruments.com>
2024-09-11 10:00:56 +0000

    setti inn smá configure fæla

--- modules/home/saumavel.nix
+++ modules/home/saumavel.nix
@@ -27,42 +27,66 @@
           enableFishIntegration = true;
         };
 
-        kitty.enable = true;
-        kitty.shellIntegration.enableFishIntegration = true;
+        # kitty.enable = true;
+        # kitty.shellIntegration.enableFishIntegration = true;
+        kitty = {
+            enable = true;
+            shellIntegration.enableFishIntegration = true;
+        };
 
-        tmux {
+        tmux = {
           enable = true;
           extraConfig = ''
+            unbind r
+            bind r source-file ~/.tmux.conf
+            
+            set -g prefix C-s
+            
+            set -g mouse on
+            
+            set -g base-index 1
+            set -g pane-base-index 1
+            
+            # act like vim
+            bind-key h select-pane -L
+            bind-key j select-pane -D
+            bind-key k select-pane -U
+            bind-key l select-pane -R
+            
+            # remappa " og % í þægilegri takka
+            bind i split-window -h
+            bind u split-window -v
+            
             set -g status-position top
 
             # List of plugins
-            set -g @plugin 'tmux-plugins/tpm'
-            set -g @plugin 'catppuccin/tmux'
+            # set -g @plugin 'tmux-plugins/tpm'
+            # set -g @plugin 'catppuccin/tmux'
 
-            # Catppuccin settings
-            set -g @catppuccin_window_left_separator ""
-            set -g @catppuccin_window_right_separator ""
-            set -g @catppuccin_window_middle_separator " █"
-            set -g @catppuccin_window_number_position "right"
-    
-            set -g @catppuccin_window_default_fill "number"
-            set -g @catppuccin_window_default_text "#W"
-    
-            set -g @catppuccin_window_current_fill "number"
-            set -g @catppuccin_window_current_text "#W"
-    
-            set -g @catppuccin_status_modules_right "directory session"
-            set -g @catppuccin_status_left_separator " "
-            set -g @catppuccin_status_right_separator ""
-            set -g @catppuccin_status_fill "icon"
-            set -g @catppuccin_status_connect_separator "no"
-    
-            set -g @catppuccin_directory_text "#{pane_current_path}"
-    
+            # # Catppuccin settings
+            # set -g @catppuccin_window_left_separator ""
+            # set -g @catppuccin_window_right_separator ""
+            # set -g @catppuccin_window_middle_separator " █"
+            # set -g @catppuccin_window_number_position "right"
+            #
+            # set -g @catppuccin_window_default_fill "number"
+            # set -g @catppuccin_window_default_text "#W"
+            #
+            # set -g @catppuccin_window_current_fill "number"
+            # set -g @catppuccin_window_current_text "#W"
+            #
+            # set -g @catppuccin_status_modules_right "directory session"
+            # set -g @catppuccin_status_left_separator " "
+            # set -g @catppuccin_status_right_separator ""
+            # set -g @catppuccin_status_fill "icon"
+            # set -g @catppuccin_status_connect_separator "no"
+            #
+            # set -g @catppuccin_directory_text "#{pane_current_path}"
+            # Þarf að skoða þetta. Meikar ekki alveg sens. 
             # Initialize TMUX plugin manager (keep this line at the very bottom of .tmux.conf)
             run '~/.tmux/plugins/tpm/tpm'
-        '';
-    };
+            '';
+        };
         atuin = {
           enable = true;
           enableFishIntegration = true;
@@ -107,8 +131,16 @@
               set fish_cursor_replace_one underscore blink
               set fish_cursor_visual      block
 
-              # Kári fiktar
-              # # Set IDF_TOOLS_PATH
+
+              # Custom aliases for Git commands
+              alias gst='git status'
+              alias ga='git add'
+              alias gcmsg='git commit -m'
+              alias gp='git push'
+              alias gl='git pull'
+              alias gco='git checkout'
+              alias gb='git branch'  
+              alias ll='ls -la'
               # set -gx IDF_TOOLS_PATH "$HOME/esp/esp-idf"
             '';
 
