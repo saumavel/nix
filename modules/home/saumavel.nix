aa6a078eb49fcb24f591ed1e3f7954fd18d88747
saumavel <saumavel@gmail.com>
2024-11-12 13:39:29 +0000

    path to ~/local/bin

--- modules/home/saumavel.nix
+++ modules/home/saumavel.nix
@@ -180,13 +180,13 @@
               set -gx INFOPATH /opt/homebrew/share/info $INFOPATH
           end
 
-          # # Add ~/.local/bin
-          # set -q PATH; or set PATH ""
-          # set -gx PATH "$HOME/.local/bin" $PATH
 
           # PATH for composer, IDF, python, composer
           # set -gx PATH /Users/saumavel/.local/share/nvim/lazy/mason.nvim/lua/mason-core/managers/composer /Users/saumavel/bin /Library/Developer/CommandLineTools/usr/bin $PATH
           set -gx IDF_TOOLS_PATH "$HOME/esp/esp-idf"
+          # Add ~/.local/bin
+          set -q PATH; or set PATH ""
+          set -gx PATH "$HOME/.local/bin" $PATH
         '';
     };
 
